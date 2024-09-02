317757a1dd374227435567959cc8a252423aa2a4
Bhavya Sri Mittapalli <bhavyasrim004@gmail.com>
2024-09-02 22:43:40 +0530

    final commit

--- Script.js
+++ Script.js
@@ -1,23 +1,23 @@
 let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
-let strting = "";
+let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button =>{
     button.addEventListener('click',(e)=>{
-       if(e.target.innerHtml == '='){
+       if(e.target.innerHTML == '='){
         string = eval(string);
         input.value = string;
        } 
-       else if(e.target == "AC"){
-        string = '';
+       else if(e.target.innerHTML == "AC"){
+        string = "";
         input.value = string;
        }
-       else if(e.target.innerHtml == "DEL"){
+       else if(e.target.innerHTML == "DEL"){
         string = string.substring(0,string.length - 1);
         input.value = string;
        }
        else{
-        string += e.target.innerHtml;
+        string += e.target.innerHTML;
         input.value = string;
        }
     })
