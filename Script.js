0ba64fed51e3ed3381d587a432786d9f20c81be0
Bhavya Sri Mittapalli <bhavyasrim004@gmail.com>
2024-09-04 19:29:10 +0530

    error message

--- Script.js
+++ Script.js
@@ -5,8 +5,27 @@ let arr = Array.from(buttons);
 arr.forEach(button =>{
     button.addEventListener('click',(e)=>{
        if(e.target.innerHTML == '='){
-        string = parseInt(string);
-        string = eval(string);
+        function validateAndSanitizeInput(input) {
+            if (input.includes('//')) {
+                return "Error in expression";
+            }
+            input = input.replace(/[^0-9+\-*/.()%]/g, '');
+            input = input.replace(/\b0+(\d)/g, '$1');
+        
+            return input;
+        }
+        
+        function calculate(expression) {
+            let sanitizedExpression = validateAndSanitizeInput(expression);
+            
+            try {
+                let result = eval(sanitizedExpression);
+                return result;
+            } catch (e) {
+                return "Error in expression";
+            }
+        }
+        string = calculate(string);
         input.value = string;
        } 
        else if(e.target.innerHTML == "AC"){
