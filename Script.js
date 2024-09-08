7df37f19f1196a80b23c7613ea1c9f26462b3c64
Bhavya Sri Mittapalli <bhavyasrim004@gmail.com>
2024-09-08 19:50:50 +0530

    removed allowing two operators

--- Script.js
+++ Script.js
@@ -2,30 +2,51 @@ let input = document.getElementById('inputBox');
 let buttons = document.querySelectorAll('button');
 let string = "";
 let arr = Array.from(buttons);
+let operators = ['+','-','*','/','%'];
 arr.forEach(button =>{
     button.addEventListener('click',(e)=>{
-       if(e.target.innerHTML == '='){
-        function validateAndSanitizeInput(input) {
-            if (input.includes('//')) {
-                return "Error in expression";
-            }
-            input = input.replace(/[^0-9+\-*/.()%]/g, '');
-            input = input.replace(/\b0+(\d)/g, '$1');
-        
-            return input;
+        if(e.target.innerHTML == '+'){
+            let str = string.slice(-1);
+            if(operators.includes(str)){
+                string = string.slice(0,-1);
+            } 
+            string += e.target.innerHTML;
+            input.value = string;
         }
-        
-        function calculate(expression) {
-            let sanitizedExpression = validateAndSanitizeInput(expression);
-            
-            try {
-                let result = eval(sanitizedExpression);
-                return result;
-            } catch (e) {
-                return "Error in expression";
+        else if(e.target.innerHTML == '-'){
+            let str = string.slice(-1);
+            if(str=='+'||str=='-'){
+                string = string.slice(0,-1);
             }
+            string += e.target.innerHTML;
+            input.value = string;
         }
-        string = calculate(string);
+        else if(e.target.innerHTML == '*'){
+            let str = string.slice(-1);
+            if(operators.includes(str) && str != '*'){
+                string = string.slice(0,-1);
+            } 
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '/'){
+            let str = string.slice(-1);
+            if(operators.includes(str)){
+                string = string.slice(0,-1);
+            } 
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        else if(e.target.innerHTML == '%'){
+            let str = string.slice(-1);
+            if(operators.includes(str)){
+                string = string.slice(0,-1);
+            } 
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+       else if(e.target.innerHTML == '='){
+        string = eval(string);
         input.value = string;
        } 
        else if(e.target.innerHTML == "AC"){
